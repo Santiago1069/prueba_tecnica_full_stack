f0450c0802cf49dfbbac1dbd679e0d59d95a3e7e
Santiago Sandoval <casaba3131@gmail.com>
2022-10-28 16:22:51 -0500

    Se inicializa el proyecto en git y se muestra un mensaje de prueba en http://localhost:3525

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 3525;
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res){
+	res.status(200).send({
+		message: "https://jsonplaceholder.typicode.com/users"
+	});
+});
+
+app.listen(port, function(){
+	console.log(`click on ==> http://localhost:${port}`);
+});
